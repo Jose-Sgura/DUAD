3238f1df3428daf61ed651d3854cd1f3f9018da1
Jose Sgura <josee9425@gmail.com>
2026-06-15 18:23:07 -0600

    ejericios 3 de trasacciones

--- Trasacción/my_project.sql
+++ Trasacción/my_project.sql
@@ -1,58 +1,28 @@
 DO $$
 DECLARE
-    v_user_id INT := 1;
-    v_bill_id INT;
-    v_stock INT;
-
+    v_bill_id INT := 4;
+    v_exists INT;
 BEGIN
 
-#
-Confirmar que el usuario que realiza la compra existe en la DB.
-    IF NOT EXISTS (SELECT 1 FROM users WHERE id = v_user_id) THEN
-        RAISE EXCEPTION 'The user with id % does not exist.', v_user_id;
-    END IF;
-
-#Comprobar si hay existencias suficientes de cada uno de los productos dentro de la factura.
-SELECT stock INTO v_stock from products WHERE id =1;
-IF v_stock < 2 THEN
-    RAISE EXCEPTION 'Not enough stock for product with id %.', v_stock;
+IF NOT EXISTS (SELECT 1 FROM bills WHERE id = v_bill_id) THEN
+    RAISE NOTICE 'The invoice % was returned before', v_bill_id;
 END IF;
 
-SELECT stock INTO v_stock from products WHERE id =3;
-IF v_stock < 4 THEN
-    RAISE EXCEPTION 'Not enough stock for product with id %.', v_stock;
-END IF;
+UPDATE products
+SET stock = stock + bi.quantity
+FROM bill_items bi
+WHERE products.id = bi.product_id
+AND bi.bill_id = v_bill_id;
 
-
-
-#Insertar la factura con el usuario relacionado
-INSERT INTO bills (user_id, total, status) 
-VALUES (v_user_id,0.00,'pending')
-RETURNING id INTO v_bill_id;
-
-INSERT INTO bill_item (bill_id, product_id, quantity, unit_price)
-VALUES (v_bill_id, 1, 2, 999.99),
-(v_bill_id, 3, 4, 29.99);
-
-#Reducir el stock de los productos según la cantidad comprada.
-UPDATE products SET stock  = stock - 2 WHERE id = 1;
-UPDATE products SET stock  = stock - 4 WHERE id = 3;
-
-UPDATE bills 
-SET total = (
-    SELECT SUM(quantity * unit_price)
-    FROM bill_item
-    WHERE bill_id = v_bill_id
-) 
+UPDATE bills
+SET status = 'returned'
 WHERE id = v_bill_id;
 
-RAISE NOTICE 'Buy process completed successfully. Bill ID:%', v_bill_id;
-
-
+RAISE NOTICE 'Returning proceed succesfully for the invoice %', v_bill_id;
 
 EXCEPTION
-    WHEN OTHERS THEN
-        RAISE NOTICE 'An error occurred: %', SQLERRM;
+    WHEN OTHER THEN 
+        RAISE NOTICE 'Transaction failed: %', SQLERRM;
 
 END; $$ language plpgsql;
 
