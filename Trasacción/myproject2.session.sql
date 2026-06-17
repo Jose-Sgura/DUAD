e3c005a4fd3f444759d7b40728241d07315fe46a
Jose Sgura <josee9425@gmail.com>
2026-06-17 12:53:35 -0600

    ejercicio 3 extra trasacciones

--- Trasacción/myproject2.session.sql
+++ Trasacción/myproject2.session.sql
@@ -1,70 +1,74 @@
+#se actuliza tabla prouducts para dejar stock en 1 y version también.
+UPDATE products SET stock = 1, version = 1 WHERE id = 1;
+
+#se agrega columna version a la tabla producto
+#para llevar un indicador de cambio
+ALTER TABLE products 
+ADD COLUMN version in DEFAULT 1;
+
 DO $$
 DECLARE
-    v_user_id INT := 1;
-    v_bill_id INT;
-    v_stock INT;
-
-    v_qty_p1 INT := 2;
-    v_qty_p2 INT := 3;
-    v_qty_p3 INT := 5;
+    v_user_id      INT := 1;
+    v_product_id   INT := 1;
+    v_quantity     INT := 1;
+    v_bill_id      INT;
+    v_stock        INT;
+    v_version      INT;
+    v_rows_updated INT;
 
 BEGIN
+    IF NOT EXISTS (SELECT 1 FROM users WHERE id = v_user_id) THEN
+        RAISE EXCEPTION 'The user % does not exist', v_user_id;
+    END IF;
 
-IF NOT EXISTS (SELECT 1 FROM users WHERE id = v_user_id)
-THEN RAISE EXCEPTION 'The user id % does not exist', v_user_id;
-END IF;
+    SELECT stock, version INTO v_stock, v_version
+    FROM products
+    WHERE id = v_product_id;
 
+    RAISE NOTICE 'Read stock: %, Read version: %', v_stock, v_version;
 
-SELECT stock INTO v_stock FROM products WHERE id = 1;
-IF v_stock < v_qty_p1 
-THEN RAISE EXCEPTION 'Stock is not enough for product id =1. Enable: %, Requested: %',v_stock, v_qty_p1; 
-END IF;
+    IF v_stock < v_quantity THEN
+        RAISE EXCEPTION 'The stock is not enough. Available: %, Requested: %', v_stock, v_quantity;
+    END IF;
 
-SELECT stock INTO v_stock FROM products WHERE id = 2;
-IF v_stock < v_qty_p2 
-THEN RAISE EXCEPTION 'Stock is not enough for product id =2. Enable: %, Requested: %',v_stock, v_qty_p2;
-END IF;
+    INSERT INTO bills (user_id, total, status)
+    VALUES (v_user_id, 0.00, 'pending')
+    RETURNING id INTO v_bill_id;
 
-SELECT stock INTO v_stock FROM products WHERE id = 3;
-IF v_stock < v_qty_p3 
-THEN RAISE EXCEPTION 'Stock is not enough for product id =3. Enable: %, Requested: %',v_stock, v_qty_p3;
-END IF;
+#Use una transacción que lea el stock y actualice solo si el stock no cambió en el proceso
+    
+    UPDATE products
+    SET stock   = stock - v_quantity,
+        version = version + 1
+    WHERE id      = v_product_id
+    AND   version = v_version
+    AND   stock   >= v_quantity;
 
-INSERT INTO bills (user_id, total, status)
-VALUES (v_user_id, 0.00, 'pending')
-RETURNING id INTO v_bill_id;
+    GET DIAGNOSTICS v_rows_updated = ROW_COUNT;
 
-INSERT INTO bill_item (bill_id, product_id, quantity, unit_price)
-SELECT v_bill_id, id,
-    CASE id
-        WHEN 1 THEN v_qty_p1
-        WHEN 2 THEN v_qty_p2
-        WHEN 3 THEN v_qty_p3
-    END,
-    price
-FROM products
-WHERE id IN (1,2,3);
+#Si otro proceso modificó el stock mientras tanto, debe abortar la compra
+    IF v_rows_updated = 0 THEN
+        RAISE EXCEPTION 'Aborted buy, another process modified the stock from product %', v_product_id;
+    END IF;
 
-UPDATE products SET stock = stock - v_qty_p1 WHERE id = 1;
-UPDATE products SET stock = stock - v_qty_p2 WHERE id = 2;
-UPDATE products SET stock = stock - v_qty_p3 WHERE id = 3;
+    INSERT INTO bill_item (bill_id, product_id, quantity, unit_price)
+    SELECT v_bill_id, id, v_quantity, price
+    FROM products WHERE id = v_product_id;
 
-UPDATE bills
-SET total = (
-    SELECT SUM (quantity * unit_price)
-    FROM bill_item
-    WHERE bill_id = v_bill_id
-)
+    UPDATE bills
+    SET total = (
+        SELECT SUM(quantity * unit_price)
+        FROM bill_item
+        WHERE bill_id = v_bill_id
+    )
+    WHERE id = v_bill_id;
 
-WHERE id = v_bill_id;
-
-RAISE NOTICE 'Massive buy successfully. Invoice id: %, total: %', 
-v_bill_id,
-(SELECT total FROM bills WHERE id = v_bill_id);
+    RAISE NOTICE 'Successful buy. Invoice ID: %, Product ID: %, New version: %',
+        v_bill_id, v_product_id, v_version + 1;
 
 EXCEPTION
     WHEN OTHERS THEN
-    RAISE EXCEPTION 'Transaction called off: %', SQLERRM;
+        RAISE EXCEPTION 'Transaction aborted: %', SQLERRM;
 
 END;
 $$ LANGUAGE plpgsql;
